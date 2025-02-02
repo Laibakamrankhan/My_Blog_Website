052699433d14c51b3e404d8a3cd4731e4baad51b
Laibakamrankhan <160596709+Laibakamrankhan@users.noreply.github.com>
2025-02-02 17:58:23 +0500

    Update page.tsx

--- app/Blogs/[slug]/page.tsx
+++ app/Blogs/[slug]/page.tsx
@@ -39,11 +39,11 @@ return (
   <div className="bg-gray-100 min-h-screen">
         <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg mt-10">
           <div className="text-green-700  py-6 px-8 rounded-t-lg">
-            <h1 className="text-4xl font-bold">{blogs.title}</h1>
+            <h1 className="text-4xl font-bold">{blog.title}</h1>
           </div>
           <div className="p-8">
             <div className="text-gray-800 text-lg leading-relaxed">
-              <PortableText value={blogs.blogdescription} />
+              <PortableText value={blog.blogdescription} />
             </div>
           </div>
         </div>
