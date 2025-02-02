cfe009646e3b34bb527d29c9b79ee13ceb8e2fb9
Laibakamrankhan <160596709+Laibakamrankhan@users.noreply.github.com>
2025-02-02 18:16:48 +0500

    Update page.tsx

--- app/Blogs/[slug]/page.tsx
+++ app/Blogs/[slug]/page.tsx
@@ -1,12 +1,9 @@
 import { client } from '@/sanity/lib/client';
 import { PortableText } from '@portabletext/react';
 
-interface BlogPageProps {
-  params: { slug: string };
-}
-export default async function Page({ params }: BlogPageProps) {
+
+export default async function Page({ params }: { params: { slug: string } }) {
   const { slug } = params;
-  console.log("Params:", params);
 
   let blogs = [];
 
